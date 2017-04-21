4a206cbf9b10ae48acc57735d52dca58d04dc6b1
yangdongdong1222 <yang_20161222@163.com>
2017-04-21 15:04:44 +0800

    test.py

--- www/static/test.py
+++ www/static/test.py
@@ -1,24 +1,24 @@
 
 #打印九九乘法表
-#print('\n'.join([' '.join(['%s*%s=%-2s' % (y,x,x*y) for y in range(1,x+1)]) for x in range(1,10)]))
+#print('\n'.join([' '.join(['%s*%s=%s' % (y,x,x*y) for y in range(1,x+1)]) for x in range(1,10)]))
 
 
 #画五角星
-import turtle
+# import turtle
+#
+# turtle.forward(100)
+# turtle.right(144)
+# turtle.forward(100)
+# turtle.right(144)
+# turtle.forward(100)
+# turtle.right(144)
+# turtle.forward(100)
+# turtle.right(144)
+# turtle.forward(100)
+#
+# turtle.exitonclick()
 
-turtle.forward(100)
-turtle.right(144)
-turtle.forward(100)
-turtle.right(144)
-turtle.forward(100)
-turtle.right(144)
-turtle.forward(100)
-turtle.right(144)
-turtle.forward(100)
-
-turtle.exitonclick()
-
-#画六角星
+# #画六角星
 # import turtle
 #
 # turtle.left(30)
@@ -69,3 +69,12 @@ turtle.exitonclick()
 # turtle.forward(144)
 #
 # turtle.exitonclick()
+
+
+#二重循环
+a=[1]
+num=input('请输入数值：')
+print (a)
+for i in range(int(num) -1):
+     a.append(a[-1]+1)
+     print (a)
