59e8b679ae5bc1cd978a335f0ffcffa5bc7316c0
yangdongdong1222 <yang_20161222@163.com>
2017-04-21 12:49:54 +0800

    test.py

---
+++ www/static/test.py
@@ -0,0 +1,71 @@
+
+#打印九九乘法表
+#print('\n'.join([' '.join(['%s*%s=%-2s' % (y,x,x*y) for y in range(1,x+1)]) for x in range(1,10)]))
+
+
+#画五角星
+import turtle
+
+turtle.forward(100)
+turtle.right(144)
+turtle.forward(100)
+turtle.right(144)
+turtle.forward(100)
+turtle.right(144)
+turtle.forward(100)
+turtle.right(144)
+turtle.forward(100)
+
+turtle.exitonclick()
+
+#画六角星
+# import turtle
+#
+# turtle.left(30)
+# turtle.forward(144)
+#
+# turtle.right(60)
+# turtle.forward(144)
+#
+# turtle.right(60)
+# turtle.forward(144)
+#
+# turtle.right(60)
+# turtle.forward(144)
+#
+# turtle.right(60)
+# turtle.forward(144)
+#
+# turtle.right(60)
+# turtle.forward(144)
+#
+# turtle.forward(144)
+# turtle.right(120)
+# turtle.forward(144)
+#
+# turtle.left(60)
+# turtle.forward(144)
+# turtle.right(120)
+# turtle.forward(144)
+#
+# turtle.left(60)
+# turtle.forward(144)
+# turtle.right(120)
+# turtle.forward(144)
+#
+# turtle.left(60)
+# turtle.forward(144)
+# turtle.right(120)
+# turtle.forward(144)
+#
+# turtle.left(60)
+# turtle.forward(144)
+# turtle.right(120)
+# turtle.forward(144)
+#
+# turtle.left(60)
+# turtle.forward(144)
+# turtle.right(120)
+# turtle.forward(144)
+#
+# turtle.exitonclick()
